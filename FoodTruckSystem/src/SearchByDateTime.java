8d33dc6cc5fe282aa053141b688ae0f1c83c946e
rahulanrahul <45391003+rahulanrahul@users.noreply.github.com>
2018-12-08 22:55:30 +0100

    Search By Date and Time

--- FoodTruckSystem/src/SearchByDateTime.java
+++ FoodTruckSystem/src/SearchByDateTime.java
@@ -1,97 +1,143 @@
-import java.sql.*;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class SearchByDateTime {
+public class searchByDateAndTime {
+
+	String TruckLocation;
+	String SelectedRow;
+	int TruckId;
+	int size;
+	int count;
+	boolean IsNotNumeric;
+	boolean IsNotValid;
+	String TimeOption;
 	String Date;
-	String Time;
-	private boolean flag;
-	Scanner Sc = new Scanner(System.in);
-	public static void main(String[] args) 
-	{
-		
-		try {
-			SearchByDateTime SDT =  new SearchByDateTime();
-			SDT.searchTime();
-			} catch (Exception e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 	
-		
-		 
-	}
+	String TimeSlot;
 
-	
-
-	private void searchTime() 
-	{
-		
+	public int searchTime() throws ClassNotFoundException, SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_schema?useSSL=false",
+				"root", "123456789");
+		Statement stmt = con.createStatement();
+		Scanner Sc = new Scanner(System.in);
 		do {
-		System.out.println("Please Enter Date:");
-		
-	    System.out.println("Format Date:YYYY-MM-DD");
-	    String regex = "\\d{4}-\\d{2}-\\d{2}";
-	    
-	    	Date = Sc.nextLine();   	
-	       	Pattern pattern = Pattern.compile(regex);
-	    	Matcher matcher = pattern.matcher(Date);
-	        if(matcher.matches()== false)
-	        {
-	        	System.out.print("\nPlease Enter Correcr Format : YYYY-MM-DD");
-	        		flag = false;
-	        }else
-	        {
-	        	flag = true;
-	        }
-	        }while(flag != true);
-	    
-	    System.out.println("\nPlease Select From Above Options");
-	    System.out.println("1. From 8 to 12");
-	    System.out.println("2. From 12 to 16");
-	    System.out.println("3. From 17 to 22");
-	    int Option = Sc.nextInt();
-	    if(Option == 1)
-	    {
-	    	Time = "8-12";
-	    }
-	    else if(Option == 2)
-	    {
-	    	Time = "12-16";
-	    }
-	    else
-	    {
-	    	Time = "17-22";
-	    }
-   
-	    Connection con;
+			System.out.println("Please enter From Date:(YYYY-MM-DD)");
+			Date = Sc.nextLine();
+			IsNotValid = false;
+			if (Date.contains(" ")) {
+				IsNotValid = true;
+				System.out.println("No spaces allowed. Please try again!!!");
+				continue;
+			}
+			DateFormat FromDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			FromDateFormat.setLenient(false);
+			try {
+				FromDateFormat.parse(Date);
+			} catch (ParseException ex) {
+				System.out.println("Date " + Date + " is not valid");
+				IsNotValid = true;
+			}
+		} while (IsNotValid);
+		do {
+			System.out.println("Please enter your desired timeslot:");
+			System.out.println("Please enter 1 for slot 06:00 to 10:00");
+			System.out.println("Please enter 2 for slot 12:00 to 16:00");
+			System.out.println("Please enter 3 for slot 18:00 to 22:00");
+			TimeOption = Sc.nextLine();
+			if (TimeOption.matches("\\d+")) {
+				IsNotNumeric = false;
+			} else {
+				System.out.println("You have entered a non numeric number");
+			}
+			switch (TimeOption) {
+			case "1":
+				TimeSlot = "06:00 to 10:00";
+				break;
+			case "2":
+				TimeSlot = "12:00 to 16:00";
+				break;
+			case "3":
+				TimeSlot = "18:00 to 22:00";
+				break;
+			default:
+				System.out.println("Please enter a valid input from below");
+				IsNotNumeric = true;
+			}
+		} while (IsNotNumeric);
+
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_schema","root","Abhi08338@");
-			Statement Stmt = con.createStatement();
-	        
-	    	String sql = "SELECT truck_owner_tab.TruckName, food_truck_tab.Location1,food_truck_tab.Location2,food_truck_tab.Location3, \r\n" + 
-	    			"food_truck_tab.FormDate, food_truck_tab.ToDate, food_truck_tab.TimeSlot1 , food_truck_tab.TimeSlot2 , \r\n" + 
-	    			"food_truck_tab.TimeSlot3,food_truck_tab.Days \r\n" + 
-	    			"FROM  food_truck_schema.food_truck_tab \r\n" + 
-	    			"INNER JOIN food_truck_schema.truck_owner_tab \r\n" + 
-	    			"ON food_truck_tab.TruckId = truck_owner_tab.TruckId\r\n" + 
-	    			"where(((TimeSlot1 = '8-12')||(TimeSlot2 = '12-16')||(TimeSlot3 = '17-22'))\r\n" + 
-	    			"AND(CAST('"+Date+"' AS DATETIME)BETWEEN(CAST(FormDate AS DATETIME) )AND(CAST(ToDate AS DATETIME))));";
-	    	ResultSet  resultSet = Stmt.executeQuery(sql);
-	    	System.out.println( "TruckName"  +"\t"+ "FormDate" + "\t"+ "ToDate"+ "\t"+"Location1"+"\t"+"Location2" +"\t"+"Location3" + "\t"+ "TimeSlot1"+"\t"+"TimeSlot2"+"\t"+"TimeSlot3");
-	    	 while(resultSet.next()) 
-		     { 
-		        System.out.println(resultSet.getString("TruckName")+ "\t"+resultSet.getString("FormDate")+ "\t"+resultSet.getString("ToDate")+ "\t"+ resultSet.getString("Location1")+ "\t" +resultSet.getString("Location2")+ "\t"+ resultSet.getString("Location3")+ "\t"+(resultSet.getString("TimeSlot1")+ "\t"+resultSet.getString("TimeSlot2")+ "\t"+resultSet.getString("TimeSlot3")));
-		     }
-	         
+			stmt = con.createStatement();
+			ResultSet result = stmt.executeQuery("Select truck_owner_tab.TruckName, "
+					+ "food_truck_tab.FromDate, food_truck_tab.ToDate, food_truck_tab.Days, food_truck_tab.TimeSlot1, "
+					+ "food_truck_tab.Location1, food_truck_tab.TimeSlot2, food_truck_tab.Location2, "
+					+ "food_truck_tab.TimeSlot3, food_truck_tab.Location3, truck_owner_tab.TruckId "
+					+ "From truck_owner_tab "
+					+ "inner join food_truck_tab on truck_owner_tab.TruckId = food_truck_tab.TruckId "
+					+ "where( ( TimeSlot1 = '" + TimeSlot + "' ) OR ( TimeSlot2 = '" + TimeSlot
+					+ "' ) OR ( TimeSlot3 = '" + TimeSlot + "' ) ) AND ( FromDate <= '" + Date + "' ) AND ( ToDate >= '"
+					+ Date + "' )");
+			int nCol = result.getMetaData().getColumnCount();
+			count = 0;
+			List<String[]> table = new ArrayList<>();
+			while (result.next()) {
+				size = nCol + 1;
+				String[] row = new String[size];
+				for (int iCol = 0; iCol <= nCol; iCol++) {
+					if (iCol == 0) {
+						row[iCol] = Integer.toString(++count);
+						continue;
+					}
+					Object obj = result.getObject(iCol);
+					row[iCol] = (obj == null) ? null : obj.toString();
+				}
+				table.add(row);
+			}
+			if (!table.isEmpty()) {
+				int i = 0;
+				for (String[] row : table) {
+					for (String s : row) {
+						i++;
+						if (i != (size))
+							System.out.print(s + " ");
+					}
+					i = 0;
+					System.out.println();
+					System.out.println();
+				}
+				do {
+					IsNotNumeric = false;
+					System.out.println("Please enter Food Truck Number to select");
+					SelectedRow = Sc.nextLine();
+					if (!SelectedRow.matches("\\d+") || SelectedRow.contains(" ")) {
+						System.out.println("You have entered an invalid number");
+						IsNotNumeric = true;
+					} else {
+						int rowno = Integer.parseInt(SelectedRow);
+						rowno--;
+						if ((rowno < count) && (rowno >= 0)) {
+							String Row[] = table.get(rowno);
+							TruckId = Integer.parseInt(Row[size - 1]);
+						} else {
+							System.out.println("Select a number within the range");
+							IsNotNumeric = true;
+						}
+					}
+				} while (IsNotNumeric);
+			} else {
+				TruckId = 0;
+				System.out.println("No Search results found.");
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
-    	
-		
+		return TruckId;
 	}
-
-
 }
