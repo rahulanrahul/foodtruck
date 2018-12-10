0b8a4e9e744718e8493cdd560967d3dcc250d023
mhmmdsalman <45390735+mhmmdsalman@users.noreply.github.com>
2018-12-10 07:53:12 +0100

    To run Check Appointment and Book Appointment

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 package FoodTruckSystem.Project;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		GiveFeedback Feedback = new GiveFeedback();
+		
+		
+		/*GiveFeedback Feedback = new GiveFeedback();
 		try {
 			try {
 				Feedback.CustomerFeedback();
@@ -16,9 +19,11 @@ public class Main {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		
-		}
+		}*/
 		
-		GetFeedback Feedbackdata = new GetFeedback();
+		
+		
+		/*GetFeedback Feedbackdata = new GetFeedback();
 		try {
 			try {
 				Feedbackdata.FeedbackData();
@@ -28,7 +33,58 @@ public class Main {
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		}*/
+		
+		
+		BookAppointment UserInput = new BookAppointment();
+		try {
+				UserInput.CustomerInput();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			{
+		
+						
+		BookAppointment Appointment = new BookAppointment();
+		try {
+			Appointment.CustomerAppointment();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
 		}
-}
+		
+		}
+			
+		BookAppointment BulkOrder = new BookAppointment();
+		try {
+			BulkOrder.CustomerBulkOrder();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}
+		
+		}
+		
+		CheckAppointment Appointmentdata = new CheckAppointment();
+		try {
+			Appointmentdata.Appointmentdata();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();	
+		}
+	}	
+	
+	}
 
-}
+
