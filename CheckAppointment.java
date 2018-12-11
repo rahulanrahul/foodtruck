cd82ee0bfc8ac07c42c004ee54beb58c245eacf4
mhmmdsalman <45390735+mhmmdsalman@users.noreply.github.com>
2018-12-11 16:48:38 +0100

    CheckAppointment

--- CheckAppointment.java
+++ CheckAppointment.java
@@ -1,9 +1,10 @@
-package FoodTruckSystem.Project;
+package BookAndCheckAppointment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import com.mysql.cj.jdbc.result.ResultSetImpl;
 
 public class CheckAppointment {
@@ -28,17 +29,47 @@ public class CheckAppointment {
 	public int BulkOrderId;
 	public String NoOfPerson;
 	public String Dish;
-	
-	public void Appointmentdata () throws ClassNotFoundException, SQLException {
-			
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdata","root","Shams*97");
-			java.sql.Statement stmt = con.createStatement();
-			
-			ResultSet rs = stmt.executeQuery("SELECT * FROM customer_appointment_tab where TruckId ='"+TruckId+"'");
-			System.out.println("   CustomerId   BookId TimeSlot TruckId  Location  Date  BulkOrderId  NoOfPerson  Dish");
-			while(((ResultSetImpl) rs).next()) {
-			System.out.println((" \t" + ((ResultSetImpl) rs).getString(1))+"\t "+((ResultSetImpl) rs).getString(2)+"     "+((ResultSetImpl) rs).getString(3)+"  \t "+((ResultSetImpl) rs).getInt(4)+"      \t "+((ResultSetImpl) rs).getString(5)+"\t"+((ResultSetImpl) rs).getString(6)+"\t"+((ResultSetImpl) rs).getInt(7)+"\t"+((ResultSetImpl) rs).getInt(8)+"\t"+((ResultSetImpl) rs).getString(9));
-			}	
-			
-}
+
+	public int ViewAppointmentdata() throws ClassNotFoundException, SQLException {
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_system", "root",
+				"Abhi08338@");
+		java.sql.Statement stmt = con.createStatement();
+
+		ResultSet rs = stmt.executeQuery("SELECT * FROM customer_appointment_tab where TruckId ='" + TruckId + "'");
+		// System.out.println(" CustomerId BookId TimeSlot Location Date BulkOrderId
+		// NoOfPerson Dish");
+		while (((ResultSetImpl) rs).next()) {
+			if (((ResultSetImpl) rs).getString(1) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(1)));
+			}
+			if (((ResultSetImpl) rs).getString(2) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(2)));
+			}
+			if (((ResultSetImpl) rs).getString(3) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(3)));
+			}
+			if (((ResultSetImpl) rs).getString(4) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(4)));
+			}
+			if (((ResultSetImpl) rs).getString(5) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(5)));
+			}
+			if (((ResultSetImpl) rs).getString(6) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(6)));
+			}
+			if (((ResultSetImpl) rs).getString(7) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(7)));
+			}
+			if (((ResultSetImpl) rs).getString(8) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(8)));
+			}
+			if (((ResultSetImpl) rs).getString(9) != null) {
+				System.out.print((" \t" + ((ResultSetImpl) rs).getString(9)));
+			}
+			System.out.println("");
+		}
+		return BookId;
+
+	}
 }
