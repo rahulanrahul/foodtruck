d5aed0cb9dcd65c1e175d5b36c592868993c376d
HemanthSanga <45181848+HemanthSanga@users.noreply.github.com>
2018-12-11 15:16:01 +0100

    Update UpdateOffer.java

--- UpdateOffer.java
+++ UpdateOffer.java
@@ -1,5 +1,4 @@
 package updateoffer;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,20 +6,17 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.Scanner;
-
 public class UpdateOffer {
-
-	
-	
 	public static void main(String[] args){
-	     int truckId;
+		 int truckId;
 	     String foodType;
 	     String day;
 	     String timeSlot;
-	     String offer;
-	     String from;
-	     String to;
-	     String choice ;
+		 String offer;
+		 String from;
+		 String to;
+		 String choice ;
+		 
 		try{
 				Scanner scanner = new Scanner(System.in);
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_schema","root","desertstrom47007");
@@ -28,7 +24,7 @@ public class UpdateOffer {
 				Statement stmt = con.createStatement();
 				System.out.println("Please provide TruckId :" );
 				truckId = scanner.nextInt();
-				String q = "Select * from truck_offer_tab where TruckId ='"+ truckId+"'";
+				String q = "Select * from truck_offer_tab where TruckId ='"+truckId+"'";
 				preparedStmt = con.prepareStatement(q);	
 				ResultSet rs = preparedStmt.executeQuery(q);
 				while(rs.next())
@@ -40,7 +36,7 @@ public class UpdateOffer {
 											"Offer: " +rs.getString("offer") + "\n"+
 											"From Date: " +rs.getString("fromdate") + "\n"+
 											"To Date: " +rs.getString("todate") );
-                                }
+               }
 				boolean isloopneeded;
 				do{
 					System.out.println("MENU" + "\n" +
@@ -55,7 +51,8 @@ public class UpdateOffer {
 				choice = scanner.next();
 			
 				isloopneeded = false;
-				switch(choice){
+				switch(choice)
+				{
 					case "1": {
 						
 						preparedStmt = con.prepareStatement("Delete from truck_offer_tab where TruckId ='"+ truckId+"'");	
@@ -63,52 +60,69 @@ public class UpdateOffer {
 						break;
 					}
 					case "2": {
-					
-						System.out.println("Please provide Food Type: " );
+						String oldAttribute;
+						System.out.println("Please enter the old Food Type :" );
+						oldAttribute = scanner.next();
+						System.out.println("Now provide the new Food Type: " );
 						foodType = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set foodtype ='" + foodType + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set foodtype ='" + foodType + "' where TruckId ='"+truckId+"' and FoodType = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "3": {
-					
-						System.out.println("Please provide Day: " );
+						String oldAttribute;
+						System.out.println("Please enter the old day:" );
+						oldAttribute = scanner.next();
+						System.out.println("Now provide the new day: " );
 						day = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set day ='" + day + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set day ='" + day + "' where TruckId ='"+truckId+"' and Day = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "4": {
-						
-							
-						System.out.println("Please provide Timeslot :(From-To)" );
+						String oldAttribute;
+						System.out.println("Please enter the old Timeslot:(00-00)" );
+						oldAttribute = scanner.next();
+						System.out.println("Now provide the new Timeslot:(00-00) " );
 						timeSlot = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set timeslot ='" + timeSlot + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set timeslot ='" + timeSlot + "' where TruckId ='"+truckId+"'and TimeSlot = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "5": {
-						
-						System.out.println("Please provide number of offers : " );
+						String oldAttribute;
+						System.out.println("Please enter the old Offer");
+						oldAttribute = scanner.next();
+						System.out.println("Now provide the new Offer");
 						offer = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set offer ='" + offer + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set offer ='" + offer + "' where TruckId ='"+truckId+"'and Offer = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "6": {
-						
-						System.out.println("Please provide From Date:(YYYY-MM-DD) " );
+						String oldAttribute;
+						System.out.println("Please enter the old From Date:(YYYY-MM-DD)");
+						oldAttribute = scanner.next();
+						System.out.println("Now provide new From Date:(YYYY-MM-DD) " );
 						from = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set fromdate ='" + from + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set fromdate ='" + from + "' where TruckId ='"+truckId+"'and FromDate = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "7": {
-					
-						System.out.println("Please provide To Day:(YYYY-MM-DD) " );
+						String oldAttribute;
+						System.out.println("Please enter the old to Date:(YYYY-MM-DD)");
+						oldAttribute = scanner.next();
+						System.out.println("Now provide new To Day:(YYYY-MM-DD) " );
 						to = scanner.next();
-						preparedStmt = con.prepareStatement("update truck_offer_tab set todate ='" + to + "' where TruckId ='"+truckId+"'");	
-						preparedStmt.executeUpdate();					
+						preparedStmt = con.prepareStatement("update truck_offer_tab set todate ='" + to + "' where TruckId ='"+truckId+"'and ToDate = '" +oldAttribute+"'");	
+						preparedStmt.executeUpdate();
+						System.out.println("Your Details have been sucessfully updated" );
 						break;
 					}
 					case "0":{
@@ -121,7 +135,7 @@ public class UpdateOffer {
 						break;
 					}					
 				}
-				} while(isloopneeded);
+			} while(isloopneeded);
 		}
 		catch (Exception e){
       		System.err.println("Got an exception! \n" + e);
