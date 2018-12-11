c2fcb4ba656f05cb53fcbc4745c6b9c0d2363a9c
mhmmdsalman <45390735+mhmmdsalman@users.noreply.github.com>
2018-12-11 16:48:12 +0100

    BookAppointment

--- BookAppointment.java
+++ BookAppointment.java
@@ -1,7 +1,4 @@
-package FoodTruckSystem.Project;
-
-import java.util.Date;
-import java.text.SimpleDateFormat;
+package BookAndCheckAppointment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,6 +8,8 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 import com.mysql.cj.jdbc.result.ResultSetImpl;
@@ -28,10 +27,10 @@ public class BookAppointment {
 	public String ToDate;
 	public String Reply;
 
-	public String CustomerId = "1";
+	public String CustomerId;
 	public int BookId;
 	public String TimeSlot;
-	public int TruckId = 1;
+	public int TruckId;
 	public String Location;
 	public String DateValue;
 	public String Date;
@@ -39,8 +38,9 @@ public class BookAppointment {
 	public String NoOfPerson;
 	public String Dish;
 
-	public void CustomerInput() throws ClassNotFoundException, SQLException {
-
+	public void BookAppointmentInput(int SelectedTruck, String NameFromDB) throws ClassNotFoundException, SQLException {
+		TruckId = SelectedTruck;
+		CustomerId = NameFromDB;
 		do {
 			InputMismatch = false;
 			System.out.println("Would you like to book an appointment for your order: Y or N ?");
@@ -61,60 +61,76 @@ public class BookAppointment {
 
 	public void CustomerAppointment() throws ClassNotFoundException, SQLException {
 
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdata", "root", "Shams*97");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_system", "root",
+				"Abhi08338@");
 		java.sql.Statement stmt = con.createStatement();
 		PreparedStatement preparedStmt;
 		Scanner Sc = new Scanner(System.in);
 
 		System.out.println("Please enter the below details to book an appointment:");
 
+		boolean IsNotNumeric = false;
 		do {
-			InputMismatch = false;
-
-			int i = 0;
-			String sql = "SELECT TimeSlot1,TimeSlot2,TimeSlot3, Location1, Location2, Location3 FROM projectdata.food_truck_tab where TruckId='"
-					+ TruckId + "'";
-			ResultSet RS = stmt.executeQuery(sql);
-			while (RS.next()) {
-
-				if ((RS.getString(1) != null) && (RS.getString(4) != null)) {
-					i++;
-					System.out.println(
-							"Select " + i + " for TimeSlot " + RS.getString(1) + " Location " + RS.getString(4));
-				}
-				if ((RS.getString(2) != null) && (RS.getString(5) != null)) {
-					i++;
-					System.out.println(
-							"Select " + i + " for TimeSlot " + RS.getString(2) + " Location " + RS.getString(5));
-				}
-				if ((RS.getString(3) != null) && (RS.getString(6) != null)) {
-					i++;
-					System.out.println(
-							"Select " + i + " for TimeSlot " + RS.getString(3) + " Location " + RS.getString(6));
-				}
-			}
+			System.out.println("Please enter your desired timeslot:");
+			System.out.println("Please enter 1 for slot 06:00 to 10:00");
+			System.out.println("Please enter 2 for slot 12:00 to 16:00");
+			System.out.println("Please enter 3 for slot 18:00 to 22:00");
 			TimeSlot = Sc.nextLine();
 
-			switch (TimeSlot) {
-
-			case "1":
-				break;
-
-			case "2":
-				break;
-
-			case "3":
-				break;
-
-			default:
-				InputMismatch = true;
-				System.out.println("Invalid input, please try again");
-				break;
+			if (TimeSlot.matches("\\d+")) {
+				IsNotNumeric = false;
+			} else {
+				System.out.println("You have entered a non numeric number");
 			}
-		} while (InputMismatch);
+			switch (TimeSlot) {
+			case "1":
+				TimeSlot = "06:00 to 10:00";
+				String sql1 = "SELECT TimeSlot1,Location1 FROM food_truck_tab where TimeSlot1='" + TimeSlot
+						+ "' AND TrucKId = '" + TruckId + "'";
+				ResultSet RS1 = stmt.executeQuery(sql1);
+				if (!RS1.next()) {
+
+					System.out.println("No Truck Service Available For This TimeSlot ");
+					IsNotNumeric = true;
+				} else {
+					Location = RS1.getString(2);
+				}
+
+				break;
+			case "2":
+				TimeSlot = "12:00 to 16:00";
+				String sql2 = "SELECT TimeSlot2,Location2 FROM food_truck_tab where TimeSlot2='" + TimeSlot
+						+ "' AND TrucKId = '" + TruckId + "'";
+				ResultSet RS2 = stmt.executeQuery(sql2);
+				if (!RS2.next()) {
+
+					System.out.println("No Truck Service Available For This TimeSlot ");
+					IsNotNumeric = true;
+				} else {
+					Location = RS2.getString(2);
+				}
+				break;
+			case "3":
+				TimeSlot = "18:00 to 22:00";
+				String sql3 = "SELECT TimeSlot3,Location3 FROM food_truck_tab where TimeSlot3='" + TimeSlot
+						+ "' AND TrucKId = '" + TruckId + "'";
+				ResultSet RS3 = stmt.executeQuery(sql3);
+				if (!RS3.next()) {
+
+					System.out.println("No Truck Service Available For This TimeSlot ");
+					IsNotNumeric = true;
+				} else {
+					Location = RS3.getString(2);
+				}
+				break;
+			default:
+				System.out.println("Please enter a valid input from below");
+				IsNotNumeric = true;
+			}
+		} while (IsNotNumeric);
 
 		boolean IsNotValid = false;
-		String sql = "SELECT FromDate, ToDate FROM projectdata.food_truck_tab where TruckId='" + TruckId + "'";
+		String sql = "SELECT FromDate, ToDate FROM food_truck_tab where TruckId='" + TruckId + "'";
 		ResultSet RS = stmt.executeQuery(sql);
 		while (((ResultSetImpl) RS).next()) {
 			FromDate = RS.getString(1);
@@ -142,9 +158,6 @@ public class BookAppointment {
 					System.out.println("Date " + DateValue + " is not in Range");
 					IsNotValid = true;
 				}
-				/*
-				 * if (((Dt.equals(DateFr)) || (Dt.equals(DateTo)))) { IsNotValid = false; }
-				 */
 			} catch (ParseException ex) {
 				System.out.println("Date " + DateValue + " is not valid");
 				IsNotValid = true;
@@ -160,35 +173,35 @@ public class BookAppointment {
 				CustomerBulkOrder();
 			} else if ((Reply.equals("N"))) {
 				System.out.println("Thank you for booking an appointment for your order");
-				System.exit(0);
 			} else {
 				System.out.println("Invalid Input, please enter either Y or N");
 				InputMismatch = true;
 			}
 		} while (InputMismatch);
 
-		String Query = "INSERT INTO customer_appointment_tab (`BookId`,`CustomerId`, `TimeSlot`, `TruckId`, `Location`, `Date`,`BulkOrderId`, `NoOfPerson`,`Dish`) VALUES (?,?,?,?,?,?,?,?,?)";
+		String Query = "INSERT INTO customer_appointment_tab (`TruckId`,`CustomerId`, `TimeSlot`,`Location`, `Date`,`BulkOrderId`, `NoOfPerson`,`Dish`) VALUES (?,?,?,?,?,?,?,?)";
 
 		preparedStmt = con.prepareStatement(Query);
-		preparedStmt.setInt(1, BookId);
+
+		preparedStmt.setInt(1, TruckId);
 		preparedStmt.setString(2, CustomerId);
 		preparedStmt.setString(3, TimeSlot);
-		preparedStmt.setInt(4, TruckId);
-		preparedStmt.setString(5, Location);
-		preparedStmt.setString(6, Date);
-		preparedStmt.setInt(7, BulkOrderId);
-		preparedStmt.setString(8, NoOfPerson);
-		preparedStmt.setString(9, Dish);
+		preparedStmt.setString(4, Location);
+		preparedStmt.setString(5, DateValue);
+		preparedStmt.setInt(6, BulkOrderId);
+		preparedStmt.setString(7, NoOfPerson);
+		preparedStmt.setString(8, Dish);
 
 		preparedStmt.execute();
 		con.close();
-		
+
 		System.exit(0);
 	}
 
 	public void CustomerBulkOrder() throws ClassNotFoundException, SQLException {
 
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectdata", "root", "Shams*97");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_system", "root",
+				"Abhi08338@");
 		java.sql.Statement stmt = con.createStatement();
 		PreparedStatement preparedStmt;
 		Scanner Sc = new Scanner(System.in);
@@ -211,20 +224,20 @@ public class BookAppointment {
 
 			InputMismatch = false;
 			int i = 0;
-			String sql = "SELECT Dish FROM projectdata.truckmenu_tab where TruckId='" + TruckId + "'";
+			String sql = "SELECT Dish FROM truckmenu_tab where TruckId='" + TruckId + "'";
 			ResultSet RS = stmt.executeQuery(sql);
 			while (RS.next()) {
 				if (RS.getString(1) != null) {
 					i++;
-					System.out.println("Select " + i + " for Dish " + RS.getString(1));
+					System.out.println(" " + i + " " + RS.getString(1));
 				}
-
-				Dish = Sc.nextLine();
-
 			}
-
+			System.out.println("Enter Dish From Above Option");
+			Dish = Sc.nextLine();
 		} while (InputMismatch);
-
+		Random RandomNumber = new Random();
+		BulkOrderId = RandomNumber.nextInt(100);
+		System.out.println("Your Booking is Confirmed Pls Refer This Number for Further Communication:" + BulkOrderId);
 		System.out.println("Thank you for booking an appointment for your bulk order");
 	}
 
