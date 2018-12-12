c9437b797bfef6924c658a84f378a3e0c999a499
HemanthSanga <45181848+HemanthSanga@users.noreply.github.com>
2018-12-12 12:30:08 +0100

    Update ChangeTruckInformation.java

--- ChangeTruckInformation.java
+++ ChangeTruckInformation.java
@@ -1,32 +1,29 @@
+package opppp;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
-
-public class ChangeTruckInformation {
-
+ public class ChangeTruckInformation {
     public static String NewDetails;
 	public static String TruckID;
     public static String FoodType;
     public static String Dish;
 	public static String Price;
-	public static void main(String[] args)
+	public static void main(String[] args) throws SQLException
 	{
+     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/food_truck_schema","root","desertstrom47007");
+     
      Scanner scan = new Scanner(System.in);
+     System.out.println("Enter the Truck ID");
+     TruckID = scan.next();
      System.out.println("Enter the type of details you want to change");
      NewDetails = scan.next();
      {
-     if (NewDetails.equals("TruckID"))
-        {
-         System.out.println("You have selected");
-         System.out.println(NewDetails);
-         System.out.println("Enter the new Truck ID");
-         TruckID = scan.next();
-         System.out.println("Your Details have been successfully updated");
-        }
-     else if (NewDetails.equals("FoodType"))
+    
+     if (NewDetails.equals("FoodType"))
         {
      	 System.out.println("You have selected");
      	 System.out.println(NewDetails);
@@ -53,5 +50,4 @@ public class ChangeTruckInformation {
       }
 	}
 	
-
-}
+ }
